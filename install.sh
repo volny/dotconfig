8007b5d0647eccfc336201054ba4d9ca408641b6
Felix Volny <github@volny.co>
2018-04-22 17:19:03 +0700

    install script

---
+++ install.sh
@@ -0,0 +1,57 @@
+
+# brew
+
+# TODO
+
+# xargs brew install < brew/list
+# xargs brew cask install < brew/caskList
+
+# iterm2
+
+# TODO
+
+# git
+
+ln -sf ~/.config/git/gitconfig ~/.gitconfig
+ln -sf ~/.config/git/gitignore ~/.gitignore
+
+# silver-searcher: agignore = gitignore
+
+ln -sf ~/.config/git/gitignore ~/.config/ag/ignore
+
+# zsh
+
+ln -sf ~/.config/zsh/aliases.zsh ~/.aliases
+ln -sf ~/.config/zsh/functions.zsh ~/.functions
+ln -sf ~/.config/zsh/git.zsh ~/.git-zsh-config
+ln -sf ~/.config/zsh/vim.zsh ~/.vim-zsh-config
+ln -sf ~/.config/zsh/prompt.zsh ~/.prompt
+ln -sf ~/.config/zsh/zlogin.zsh ~/.zlogin
+ln -sf ~/.config/zsh/zlogout.zsh ~/.zlogout
+ln -sf ~/.config/zsh/zpreztorc.zsh ~/.zpreztorc
+ln -sf ~/.config/zsh/zprofile.zsh ~/.zprofile
+ln -sf ~/.config/zsh/zshenv.zsh ~/.zshenv
+ln -sf ~/.config/zsh/zshrc.zsh ~/.zshrc
+ln -sf ~/.config/zsh/zsh-autosuggestions.zsh ~/.zsh-autosuggestions
+ln -sf ~/.config/zsh/keybindings.zsh ~/.keybindings
+
+# javascript
+
+ln -sf ~/.config/javascript/editorconfig ~/.editorconfig
+ln -sf ~/.config/javascript/prettierignore ~/.prettierignore
+ln -sf ~/.config/javascript/eslintignore ~/.eslintignore
+ln -sf ~/.config/javascript/eslintrc ~/.eslintrc
+ln -sf ~/.config/javascript/prettierrc ~/.prettierrc
+
+# install global JS dependencies
+cd ~/.config/yarn/global && yarn
+
+# tmux
+
+ln -sf ~/.config/tmux/tmux.conf ~/.tmux.conf
+ln -sf ~/.config/tmux/tmux.conf.local ~/.tmux.conf.local
+
+# nvim
+
+# TODO
+

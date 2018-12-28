74adfcd0ac047087147d1657696fa2af87a94ef1
Felix Volny <github@volny.co>
2018-12-28 18:09:49 +0800

    bind ctrl-o to open vim from fzf; show file preview

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -16,4 +16,8 @@ source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 # https://github.com/junegunn/fzf#respecting-gitignore
 export FZF_DEFAULT_COMMAND='fd --type f'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+# ctrl-o to open file in vim & always show preview
+export FZF_DEFAULT_OPTS="--bind='ctrl-o:execute(nvim {})+abort' --preview 'bat --color \"always\" {}'"
+# don't show preview
+# export FZF_DEFAULT_OPTS="--bind='ctrl-o:execute(nvim {})+abort'"
 
